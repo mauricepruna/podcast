e78a72727c6857a067a531e5b4c5ef96e65d533a
Maurice Pruna <maurice@esnoei.com>
2020-03-03 21:41:18 -0500

    Adding policyDoc

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,9 +1,30 @@
 resource "aws_s3_bucket" "audio_repository" {
   bucket = "mp3-repository-bucket"
-  acl    = "private"
-
+  acl    = "public-read"
+  policy = data.aws_iam_policy_document.audio_repository_policy.json
   tags = {
     Name        = "audio repository"
 
   }
+}
+data "aws_iam_policy_document" "audio_repository_policy" {
+  statement {
+    sid = "PublicRead"
+
+    actions = [
+      "s3:GetObject"
+    ]
+
+    resources = ["arn:aws:s3:::mp3-repository-bucket/*"]
+  }
+
+  # statement {
+  #   actions = [
+  #     "s3:ListBucket",
+  #   ]
+
+  #   resources = [
+  #     "arn:aws:s3:::mp3-repository-bucket/*"
+  #   ]
+  #   }
 }
