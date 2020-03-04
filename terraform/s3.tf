99c0e05225e3403e0732fdbe199be45c3d142bca
Maurice Pruna <maurice@esnoei.com>
2020-03-04 00:52:01 -0500

    adding policy to s3

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,30 +1,32 @@
-resource "aws_s3_bucket" "audio_repository" {
-  bucket = "mp3-repository-bucket"
-  acl    = "public-read"
-  policy = data.aws_iam_policy_document.audio_repository_policy.json
-  tags = {
-    Name        = "audio repository"
-
-  }
+variable "bucket_name" {
+  type = string
+  default = "mp3-repository-bucket"
 }
-data "aws_iam_policy_document" "audio_repository_policy" {
-  statement {
-    sid = "PublicRead"
 
-    actions = [
-      "s3:GetObject"
-    ]
-
-    resources = ["arn:aws:s3:::mp3-repository-bucket/*"]
-  }
-
-  # statement {
-  #   actions = [
-  #     "s3:ListBucket",
-  #   ]
-
-  #   resources = [
-  #     "arn:aws:s3:::mp3-repository-bucket/*"
-  #   ]
-  #   }
+resource "aws_s3_bucket" "audio_repository" {
+  bucket = var.bucket_name
+  acl    = "public-read"
+  policy = <<EOF
+{
+ 
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "bucket_policy_site_main",
+      "Action": [
+        "s3:GetObject"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::${var.bucket_name}/*",
+      "Principal": "*"
+    }
+  ]
+}
+EOF
+    website {
+        index_document = "index.html"
+        error_document = "404.html"
+    }
+    
+    force_destroy = true
 }
