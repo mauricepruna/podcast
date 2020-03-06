16061f5fbb54b88877a8d48185a7489972683385
Maurice Pruna <maurice@esnoei.com>
2020-03-05 22:32:05 -0500

    Adding index.html file

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -3,6 +3,11 @@ variable "bucket_name" {
   default = "mp3-repository-bucket"
 }
 
+variable "path_to_index"{
+  type = string
+  default = "files/index.html"
+}
+
 resource "aws_s3_bucket" "audio_repository" {
   bucket = var.bucket_name
   acl    = "public-read"
@@ -11,14 +16,24 @@ resource "aws_s3_bucket" "audio_repository" {
  
   "Version": "2012-10-17",
   "Statement": [
+     {
+      "Action": [
+        "s3:ListBucket"
+      ],
+      "Effect": "Allow",
+       "Principal": "*",
+      "Resource": [
+        "arn:aws:s3:::${var.bucket_name}"
+      ]
+    },
     {
-      "Sid": "bucket_policy_site_main",
+   
       "Action": [
         "s3:GetObject"
       ],
       "Effect": "Allow",
-      "Resource": "arn:aws:s3:::${var.bucket_name}/*",
-      "Principal": "*"
+        "Principal": "*",
+      "Resource": "arn:aws:s3:::${var.bucket_name}/*"
     }
   ]
 }
@@ -30,3 +45,22 @@ EOF
     
     force_destroy = true
 }
+
+
+resource "aws_s3_bucket_object" "index_file" {
+  bucket = var.bucket_name
+  key    = "index.html"
+  source = var.path_to_index
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5(var.path_to_index)
+  depends_on = [aws_s3_bucket.audio_repository]
+}
+
+output "audio_repository_endpoint" {
+  value = aws_s3_bucket.audio_repository.website_endpoint
+}
+
+
